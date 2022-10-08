3ce4ba6b2f172372a365ab32a7c50669f4937123
rae-eun <didfodms@gmail.com>
2022-10-08 12:24:20 +0900

    edit some

---
+++ character-component.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent("character", {
+  schema: {},
+  init: function () {},
+  update: function () {},
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {},
+});
