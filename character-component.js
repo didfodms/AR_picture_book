c4d367ea954c3f05ac022d7be8283c7c0a3d4bc3
reyang <didfodms@gmail.com>
2022-10-08 15:53:41 +0900

    images edit

--- character-component.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent("character", {
-  schema: {},
-  init: function () {},
-  update: function () {},
-  tick: function () {},
-  remove: function () {},
-  pause: function () {},
-  play: function () {},
-});
