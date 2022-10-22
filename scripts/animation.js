7dc2ddca8363f35975232d8446f6f0f1d705d739
reyang <didfodms@gmail.com>
2022-10-23 02:26:03 +0900

    animation.js soundhandler.js edit

---
+++ scripts/animation.js
@@ -0,0 +1,63 @@
+// animation-jump //
+AFRAME.registerComponent("animation-jump", {
+  init: function () {
+    this.entity = this.el;
+
+    this.entity.setAttribute("animation__jump", {
+      property: "position",
+      to: "0 5 0.5",
+      dir: "alternate",
+      dur: "1000",
+      loop: "true",
+      easing: "easeInOutQuad",
+    });
+  },
+});
+
+// animation-scale //
+AFRAME.registerComponent("animation-scale", {
+  init: function () {
+    this.entity = this.el;
+
+    this.entity.setAttribute("animation__scale", {
+      property: "scale",
+      to: "6 5 5",
+      dir: "alternate",
+      dur: "1000",
+      loop: "true",
+      easing: "easeInOutQuad",
+    });
+  },
+});
+
+// animation-rotation-y //
+AFRAME.registerComponent("animation-rotation-y", {
+  init: function () {
+    this.entity = this.el;
+
+    this.entity.setAttribute("animation__rotation", {
+      property: "rotation",
+      from: "0 0 0",
+      to: "0 360 0",
+      dur: "1000",
+      loop: "true",
+      easing: "linear",
+    });
+  },
+});
+
+// animation-rotation-z //
+AFRAME.registerComponent("animation-rotation-z", {
+  init: function () {
+    this.entity = this.el;
+
+    this.entity.setAttribute("animation__rotation", {
+      property: "rotation",
+      from: "0 0 0",
+      to: "360 0 0",
+      dur: "1000",
+      loop: "true",
+      easing: "linear",
+    });
+  },
+});
