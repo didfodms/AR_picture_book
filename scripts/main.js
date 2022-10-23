290a3b25183e8928f92c8094ae7eb00b79a8af52
reyang <didfodms@gmail.com>
2022-10-23 13:42:53 +0900

    sample create

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
-const fs = require("fs");
-const dir = "./assets/book1/page1/";
+/*
+assets id : b${n}-p${n}-{bg, ob, so}
+sound id : sound-b${n}-p${n}
+marker id : marker-b${n}-p${n}
+soundHandler : soundhandler${book}-${page}
+*/
+// constant //
+const bookLength = 1;
+const bookPageLength = 3;
 
-try {
-  fs.readdir(dir, (err, files) => {
-    console.log(files.length);
-  });
-} catch (err) {
-  console.log(err);
+// element //
+const sceneEl = document.querySelector("a-scene");
+
+// create element //
+for (let bookCount = 1; bookCount <= bookLength; bookCount++) {
+  for (
+    let bookPageCount = 1;
+    bookPageCount <= bookPageLength;
+    bookPageCount++
+  ) {
+    // marker element create code
+    const marker = document.createElement("a-marker");
+    marker.id = `marker-b${bookCount}-p${bookPageCount}`;
+    marker.setAttribute("src", {});
+  }
 }
